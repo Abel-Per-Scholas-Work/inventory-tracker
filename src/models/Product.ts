5d23ae322cf777b4b49fcbe68d56b0cf8be7c13d
belunatic <abelluvyou@yahoo.com>
2025-10-02 17:11:22 -0700

    add the product class

---
+++ src/models/Product.ts
@@ -0,0 +1,19 @@
+export default class Product {
+	private sku: string;
+	public name: string;
+	public price: number;
+
+	constructor(sku: string, name: string, price: number) {
+		this.sku = sku;
+		this.name = name;
+		this.price = price;
+	}
+
+	displayDetails(): string {
+		return `The product ${this.name} (SKU: ${this.sku}) is priced at ${this.price}`;
+	}
+
+	getPriceWithTax(): number {
+		return this.price;
+	}
+}
