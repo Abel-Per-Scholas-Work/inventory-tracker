50ce41cfe2b97a26306c530775e43ae8cd3591d7
belunatic <abelluvyou@yahoo.com>
2025-10-02 18:31:28 -0700

    added discountableProduct interface and applyDiscount method

--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,15 +1,44 @@
 import Product from "./Product";
 
-export default class PhysicalProduct extends Product {
-	weight: number;
+//inteface
+interface DiscountableProduct {
+	applyDiscount(): number;
+}
 
-	constructor(sku: string, name: string, price: number, weight: number) {
+export default class PhysicalProduct
+	extends Product
+	implements DiscountableProduct
+{
+	weight: number;
+	discountInDollars?: number;
+
+	constructor(
+		sku: string,
+		name: string,
+		price: number,
+		weight: number,
+		discountInDollars?: number
+	) {
 		super(sku, name, price);
 		this.weight = weight;
+		this.discountInDollars = discountInDollars;
 	}
 
 	getPriceWithTax(): number {
 		return this.price + this.price * 0.1;
+	}
+
+	//apply discount
+	applyDiscount(): number {
+		//check to see if discount is given
+		if (this.discountInDollars) {
+			let discountPrice = this.price - this.discountInDollars;
+			return discountPrice + this.discountInDollars * 0.1;
+		}
+		// or else  the normal price
+		else {
+			return this.getPriceWithTax();
+		}
 	}
 
 	get getWeight(): string {
