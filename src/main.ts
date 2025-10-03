6ae34f512a0893519712b6c6d0cc79da34475b00
belunatic <abelluvyou@yahoo.com>
2025-10-02 19:45:49 -0700

    add discount products and login to console log the total

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import type Product from "./models/Product";
 import { calculateTax } from "./utils/taxCalculator";
 
 let sku = (): string => `PP-${Math.floor(Math.random() * 1000)}`;
+
 //Physical Product
 const bag = new PhysicalProduct(sku(), "LV bag", 3000, 2);
+
+//physical Product with Discount
+const DiscountChair = new PhysicalProduct(sku(), "Plumbing Tape", 100, 1); //default dicsount
+const DiscountTable = new PhysicalProduct(sku(), "Plumbing Tape", 200, 1, 20); //20 dollars of
 
 //digital product
 const blackMailFile = new DigitalProduct(sku(), "Dirty Laundry", 100, 30);
@@ -26,7 +31,17 @@ console.log("Utility Tax Calculator");
 console.log("This bag cost: ", calculateTax(bag));
 console.log("This file cost: ", calculateTax(blackMailFile));
 
-//display all products
-let allProduct: Product[] = [bag, blackMailFile];
+//Array of all products
+let allProduct: Product[] = [bag, blackMailFile, DiscountChair, DiscountTable];
+let TotalPrice = 0;
 
+//calculate the total
+allProduct.forEach((product) => {
+	console.log(product.getPriceWithTax());
+	return (TotalPrice += product.getPriceWithTax());
+});
+
+//display the product details
 allProduct.forEach((product) => console.log(product.displayDetails()));
+//display the total
+console.log(`Total price: $${TotalPrice}`);
